11b1e8f15cb7a36212ed1b89415e386331e375b4
qedson09 <qedson@gmail.com>
2025-08-03 18:51:39 -0300

    inclusao dos usecases de cdbbposfixado

---
+++ cdbservice/cdbservice/Core/UseCases/CalcularCdbPosFixadoCommand.cs
@@ -0,0 +1,11 @@
+﻿using cdbservice.Core.Domain;
+using MediatR;
+
+namespace cdbservice.Core.UseCases
+{
+    public class CalcularCdbPosFixadoCommand : IRequest<RetornoInvestimento>
+    {
+        public decimal ValorInicial { get; set; }
+        public int Meses { get; set; }
+    }
+}
---
+++ cdbservice/cdbservice/Core/UseCases/CalcularCdbPosFixadoCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using cdbservice.Core.Domain;
+using MediatR;
+
+namespace cdbservice.Core.UseCases
+{
+    public class CalcularCdbPosFixadoCommandHandler(
+        IIndicadorFinanceiro indicadorRepository,
+        ITaxaTbRepository taxaBaseRepository,
+        ITaxaImpostoRendaPadraoRepository taxaImpostoRepository) : IRequestHandler<CalcularCdbPosFixadoCommand, RetornoInvestimento>
+    {
+        private readonly IIndicadorFinanceiro _indicadorRepository = indicadorRepository;
+        private readonly ITaxaTbRepository _taxaBaseRepository = taxaBaseRepository;
+        private readonly ITaxaImpostoRendaPadraoRepository _taxaImpostoRepository = taxaImpostoRepository;
+
+        public Task<RetornoInvestimento> Handle(CalcularCdbPosFixadoCommand request, CancellationToken cancellationToken)
+        {
+            var taxaCdi = _indicadorRepository.ObterValor();
+            var taxaBase = _taxaBaseRepository.ObterValor();
+            var taxaImposto = _taxaImpostoRepository.ObterAliquotaPorPrazo(request.Meses);
+
+            var cdb = new CdbPosFixado(request.ValorInicial, taxaCdi, taxaBase, taxaImposto);
+            var resultado = cdb.CalcularRetornoInvestimento(request.Meses);
+            return Task.FromResult(resultado);
+        }
+    }
+}
